24684a0c0b966d0ccea24262b910882cb52fcfed
zilous1 <ck1.jeong@gmail.com>
2026-02-04 21:33:04 +0900

    Delete src directory

--- src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import { NoteApp } from "./components/NoteApp";
-
-export default function App() {
-  return (
-    <main className="min-h-screen bg-gray-50">
-      <NoteApp />
-    </main>
-  );
-}
